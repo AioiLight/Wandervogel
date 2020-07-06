0fe7d2a30137d956196efb5b190054914cd44fdd
AioiLight <info@aioilight.space>
2020-07-07 00:34:29 +0900

    カテゴリとタグ

--- index.php
+++ index.php
@@ -28,14 +28,38 @@
                         </a>
                         <?php endwhile; endif; ?>
                     </div>
+
+                    <?php posts_nav_link(' ', '<span class="button theme">新しめの投稿</span>', '<span class="button theme">古めの投稿</span>'); ?>
                 <?php else: ?>
                     <?php if(have_posts()): while(have_posts()): the_post(); ?>
                     <article>
                         <h2 class="post-title"><?php the_title(); ?></h2>
+                        
+                        <?php
+                            $category = get_the_category();
+                            $html = '<div class="post-category">カテゴリ: ';
+                            $html .= '<a href="' . get_category_link( $category[0]-> term_id ) . '" title="' . $category[0]->cat_name . '"><span class="tag theme">';
+                            $html .= $category[0]->cat_name . '</span></a>';
+                            echo $html;
+                        ?>
+                        <?php
+                            $tags = get_tags();
+                            $html = '<div class="post-tags">タグ: ';
+                            foreach ( $tags as $tag ) {
+                                $tag_link = get_tag_link( $tag->term_id );
+                                        
+                                $html .= '<a href="' . $tag_link . '" title="' . $tag->name . '"><span class="tag theme">';
+                                $html .= $tag->name . '</span></a>';
+                            }
+                            $html .= '</div>';
+                            echo $html;
+                        ?>
 
                         <?php the_content(); ?>
                     </article>
                     <?php endwhile; endif; ?>
+                <?php previous_post_link( '%link', '<span class="button theme">前: %title</span>'); ?>
+                <?php next_post_link( '%link', '<span class="button theme">次: %title</span>'); ?>
                 <?php endif; ?>
             </main>
                 
