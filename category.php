00e9e1f1bc75abead9ecc9d5bd30dae53c29085b
AioiLight <info@aioilight.space>
2020-07-08 00:36:05 +0900

    タグ、カテゴリの説明

--- category.php
+++ category.php
@@ -16,17 +16,22 @@
 
         <div class="body">
             <main class="grid-item">
-                <div class="index-grid header-link">
-                    <?php if(have_posts()): while(have_posts()): the_post(); ?>
-                    <a href="<?php the_permalink( $post ); ?>">
-                        <article class="article-card">
-                            <h2><?php the_title(); ?></h2>
-                            <p><?php the_date(); ?></p>
-                            <p><?php the_excerpt(); ?></p>
-                        </article>
-                    </a>
-                    <?php endwhile; endif; ?>
-                </div>
+                <section>
+                    <h2>カテゴリ: <?php single_cat_title(); ?></h2>
+                    <p><?php echo category_description(); ?></p>
+
+                    <div class="index-grid header-link">
+                        <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                        <a href="<?php the_permalink( $post ); ?>">
+                            <article class="article-card">
+                                <h3><?php the_title(); ?></h3>
+                                <p><?php the_date(); ?></p>
+                                <p><?php the_excerpt(); ?></p>
+                            </article>
+                        </a>
+                        <?php endwhile; endif; ?>
+                    </div>
+                </section>
 
                 <?php posts_nav_link(' ', '<span class="button theme">新しめの投稿</span>', '<span class="button theme">古めの投稿</span>'); ?>
             </main>
