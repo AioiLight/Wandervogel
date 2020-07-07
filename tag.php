83016ac9712e371659477ffdeaae5c5edf79cd0c
AioiLight <info@aioilight.space>
2020-07-08 00:28:41 +0900

    カテゴリ、タグページ

---
+++ tag.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo( 'name' ); ?></title>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+        <?php wp_head(); ?>
+    </head>
+
+    <body <?php body_class(); ?>>
+        <header>
+            <h1 class="blog-title header-link"><a href="<?php bloginfo('url'); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+            <p class="blog-description"><?php bloginfo( 'description' ); ?></p>
+        </header>
+
+        <div class="body">
+            <main class="grid-item">
+                <div class="index-grid header-link">
+                    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                    <a href="<?php the_permalink( $post ); ?>">
+                        <article class="article-card">
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_date(); ?></p>
+                            <p><?php the_excerpt(); ?></p>
+                        </article>
+                    </a>
+                    <?php endwhile; endif; ?>
+                </div>
+
+                <?php posts_nav_link(' ', '<span class="button theme">新しめの投稿</span>', '<span class="button theme">古めの投稿</span>'); ?>
+            </main>
+                
+            <div class="side grid-item">
+                <?php dynamic_sidebar(); ?>
+            </div>
+        </div>
+
+        <footer>
+            <p class="blog-copyright">(c) 2020 Author</p>
+            <p class="blog-powered">Powered by WordPress and Love &lt;3 </p>
+        </footer>
+        <?php wp_footer(); ?>
+    </body>
+</html>
