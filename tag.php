1d52834d815a3c8b6dc63c5393df6c6dd86abc9e
AioiLight <info@aioilight.space>
2020-07-08 00:38:49 +0900

    get_footer();

--- tag.php
+++ tag.php
@@ -39,11 +39,6 @@
                 <?php dynamic_sidebar(); ?>
             </div>
         </div>
-
-        <footer>
-            <p class="blog-copyright">(c) 2020 Author</p>
-            <p class="blog-powered">Powered by WordPress and Love &lt;3 </p>
-        </footer>
-        <?php wp_footer(); ?>
+        <?php get_footer(); ?>
     </body>
 </html>
