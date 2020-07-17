5a420f616e625bc6bc893824bf207dcda4a0def1
AioiLight <info@aioilight.space>
2020-07-18 00:44:49 +0900

    OGP対応

--- header.php
+++ header.php
@@ -4,6 +4,16 @@
         <title><?php wp_title(' | ', true, 'right'); ?><?php bloginfo( 'name' ); ?></title>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+        <meta property="og:url" content="<?php echo is_singular() ? get_permalink() : home_url(); ?>">
+        <meta property="og:title" content="<?php echo wp_title( '' ); ?>">
+        <meta property="og:type" content="<?php echo (is_front_page() || is_home()) ? 'website' : 'article'; ?>">
+        <meta property="og:description" content="<?php echo is_singular() ? get_the_excerpt() : get_bloginfo('description'); ?>">
+        <meta property="og:image" content="<?php echo is_singular() && has_post_thumbnail() ? wp_get_attachment_image_src( get_post_thumbnail_id(), 'full')[0] : get_stylesheet_directory_uri() . '/images/eyecatch.png'; ?>">
+        
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@aioilight">
+        <meta property="og:locale" content="ja_JP">
+
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
         <?php wp_head(); ?>
     </head>
