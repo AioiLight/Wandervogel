cc2a35d1bf24b58b15ff477bc6d479e831b529f8
AioiLight <info@aioilight.space>
2023-06-20 15:39:41 +0900

    * bump versions

--- functions.php
+++ functions.php
@@ -27,14 +27,14 @@ add_filter( 'big_image_size_threshold', '__return_false' );
 add_action("wp_enqueue_scripts", function () {
 	wp_dequeue_style( 'global-styles' );
 
-	wp_enqueue_style( 'wandervogel-common', get_stylesheet_directory_uri() . '/css/common.css', array(), '4.1');
+	wp_enqueue_style( 'wandervogel-common', get_stylesheet_directory_uri() . '/css/common.css', array(), '4.2');
 
 	if (is_archive() || is_home()) {
-		wp_enqueue_style( 'wandervogel-archive', get_stylesheet_directory_uri() . '/css/archive.css', array(), '4.1');
+		wp_enqueue_style( 'wandervogel-archive', get_stylesheet_directory_uri() . '/css/archive.css', array(), '4.2');
 	}
 
 	if (is_singular()) {
-		wp_enqueue_style( 'wandervogel-singular', get_stylesheet_directory_uri() . '/css/singular.css', array(), '4.1');
+		wp_enqueue_style( 'wandervogel-singular', get_stylesheet_directory_uri() . '/css/singular.css', array(), '4.2');
 	}
 });
 
