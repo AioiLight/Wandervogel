a51c65cf9fb02b16d840b8915c321c1735b61e15
AioiLight <info@aioilight.space>
2024-07-22 01:44:37 +0900

    * changes to nicer spacing

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p class="blog-copyright">(c) <?php bloginfo('name'); ?></p>
+    <p class="blog-copyright">&copy; <?php bloginfo('name'); ?></p>
     <p class="blog-powered">Powered by WordPress and Love &lt;3 </p>
 </footer>
 <?php wp_footer(); ?>
